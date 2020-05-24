22bc881f386aba5218ec092df6fc0e8346ad5d45
Gurpreet <gurpreet1161@gmail.com>
2020-05-24 23:10:25 +1200

    make it better

---
+++ jagtari/src/App.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       h:''
+    }
+  }
+  render() {
+    const data = () =>{
+      axios.get('/api')
+      .then(response =>
+        {
+          this.setState({
+            h : response.data.message
+          })
+          console.log(response.data.message)})
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    return (
+      <div className="App">
+      <button onClick={data}>Btn</button>
+          <h1>{this.state.h}</h1>
+    </div>
+    )
+  }
+}
+
+export default App
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const axios = require('axios')
+ const path = require('path')
+
+// const items = require('./routes/api/items');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// const uri = process.env.COOL;
+const uri = 'mongodb+srv://Gurpreet:Singh@cluster0-e7gcr.gcp.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+
+app.get('/api',(req,res) =>{
+    const g = {"message": "Hello json"}
+  res.send(g)
+})
+
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('jagtari/build'));
+
+  app.get('*',(req,res) =>{
+        res.sendFile(path.resolve(__dirname,'jagtari','build','index.html'))
+  })
+}
+
+
+mongoose.connect(uri)
+.then(()=> console.log('mongoDb connected'))
+.catch(err => console.log(err));
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port , () => console.log(`server started on ${port}`))
