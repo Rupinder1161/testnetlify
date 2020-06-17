13e2ba31475eea3120f2c11cd154c8caaeb70148
Gurpreet <gurpreet1161@gmail.com>
2020-06-17 14:58:50 +1200

    first commit

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const cors = require('cors');
 const axios = require('axios')
  const path = require('path')
 
-// const items = require('./routes/api/items');
+
 
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-// const ul = process.env.MONGO
-// console.log(typeof(ul))
-const uri = 'mongodb+srv://Gurpreet:Singh@cluster0-e7gcr.gcp.mongodb.net/test?retryWrites=true&w=majority'
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+const uri = require('./config/index').MONGO_URI
+
+mongoose.connect(uri, { useNewUrlParser:true, useCreateIndex: true, useUnifiedTopology:true }
 );
 
 const port = process.env.PORT || 5000;
@@ -31,13 +30,13 @@ mongoose.connect(uri)
  const excerciseRouter = require('./routes/excercises');
  const FoodRouter = require('./routes/food')
  app.use('/excercises',excerciseRouter);
-app.use('/food',FoodRouter)
+ app.use('/food',FoodRouter)
 
 
-app.get('/api',(req,res) =>{
-  const g = [{"message": "Hello json"},{"message": "Hello json"},{"message": "Hello json"},{"message": "Hello json"}]
-res.send(g)
-})
+// app.get('/api',(req,res) =>{
+//   const g = [{"message": "Hello json"},{"message": "Hello json"},{"message": "Hello json"},{"message": "Hello json"}]
+// res.send(g)
+// })
 
 app.use(express.static(path.join(__dirname, 'jagtari/build')));
 
@@ -45,8 +44,7 @@ app.use(express.static(path.join(__dirname, 'jagtari/build')));
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('jagtari/build'));
   
-  app.get('*',(req,res) =>{
-         
+  app.get('*',(req,res) =>{      
         res.sendFile(path.join(__dirname,'/jagtari/build/index.html'))
   })
   }
